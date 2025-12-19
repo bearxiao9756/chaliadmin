9a362a85e27d608c2a0d161f26e6835e374b321b
bearxiao9756 <bearxiao9@gmail.com>
2025-12-19 10:51:13 +0800

    Update teamgramauths.go

--- server/model/example/request/teamgramauths.go
+++ server/model/example/request/teamgramauths.go
@@ -1,23 +1,22 @@
-
 package request
 
 import (
+	"time"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/common/request"
-	
 )
 
-type AuthsSearch struct{
-      Id  *int `json:"id" form:"id"` 
-      AuthKeyId  *int `json:"authKeyId" form:"authKeyId"` 
-      LangPack  *string `json:"langPack" form:"langPack"` 
-      ApiId  *int `json:"apiId" form:"apiId"` 
-      ClientIp  *string `json:"clientIp" form:"clientIp"` 
-      DeviceModel  *string `json:"deviceModel" form:"deviceModel"` 
-      SystemLangCode  *string `json:"systemLangCode" form:"systemLangCode"` 
-      SystemVersion  *string `json:"systemVersion" form:"systemVersion"` 
-      AppVersion  *string `json:"appVersion" form:"appVersion"` 
-      Proxy  *string `json:"proxy" form:"proxy"` 
-      CreatedAtRange  []time.Time  `json:"createdAtRange" form:"createdAtRange[]"`
-      UpdatedAtRange  []time.Time  `json:"updatedAtRange" form:"updatedAtRange[]"`
-    request.PageInfo
+type AuthsSearch struct {
+	Id             *int        `json:"id" form:"id"`
+	AuthKeyId      *int        `json:"authKeyId" form:"authKeyId"`
+	LangPack       *string     `json:"langPack" form:"langPack"`
+	ApiId          *int        `json:"apiId" form:"apiId"`
+	ClientIp       *string     `json:"clientIp" form:"clientIp"`
+	DeviceModel    *string     `json:"deviceModel" form:"deviceModel"`
+	SystemLangCode *string     `json:"systemLangCode" form:"systemLangCode"`
+	SystemVersion  *string     `json:"systemVersion" form:"systemVersion"`
+	AppVersion     *string     `json:"appVersion" form:"appVersion"`
+	Proxy          *string     `json:"proxy" form:"proxy"`
+	CreatedAtRange []time.Time `json:"createdAtRange" form:"createdAtRange[]"`
+	UpdatedAtRange []time.Time `json:"updatedAtRange" form:"updatedAtRange[]"`
+	request.PageInfo
 }
